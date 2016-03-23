baabce385f41684834f3ba2f9fb2d4019a0c3502
Kefu Chai <kchai@redhat.com>
2016-03-23 14:38:06 +0800

    mon: print log if prepare_new_pool() fails
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -3602,16 +3602,21 @@ int OSDMonitor::prepare_new_pool(MPoolOp *m)
   string erasure_code_profile;
   stringstream ss;
   string ruleset_name;
+  int ret;
   if (m->auid)
-    return prepare_new_pool(m->name, m->auid, m->crush_rule, ruleset_name,
-			    0, 0,
-                            erasure_code_profile,
-			    pg_pool_t::TYPE_REPLICATED, 0, ss);
+    ret = prepare_new_pool(m->name, m->auid, m->crush_rule, ruleset_name,
+			   0, 0,
+			   erasure_code_profile,
+			   pg_pool_t::TYPE_REPLICATED, 0, ss);
   else
-    return prepare_new_pool(m->name, session->auid, m->crush_rule, ruleset_name,
-			    0, 0,
-                            erasure_code_profile,
-			    pg_pool_t::TYPE_REPLICATED, 0, ss);
+    ret = prepare_new_pool(m->name, session->auid, m->crush_rule, ruleset_name,
+			   0, 0,
+			   erasure_code_profile,
+			   pg_pool_t::TYPE_REPLICATED, 0, ss);
+  if (ret) {
+    dout(10) << __func__ << ": " << ss.str() << dendl;
+  }
+  return ret;
 }
 
 int OSDMonitor::crush_rename_bucket(const string& srcname,
